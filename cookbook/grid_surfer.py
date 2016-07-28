40268cfd0f6ba686af292c5eec4aa09c4569a536
Leonardo Uieda <leouieda@gmail.com>
2016-07-28 10:19:09 -0300

    Moved the load_surfer function to datasets module
    It's a better home for it. It's not tested or anything. Leaving this for
    the refactoring of datasets later on.
    
    Updated the cookbook recipe that uses this function.

--- cookbook/grid_surfer.py
+++ cookbook/grid_surfer.py
@@ -1,23 +1,21 @@
 """
 Gridding: Load a Surfer ASCII grid file
 """
-from fatiando import datasets, gridder
-from fatiando.vis import mpl
+from fatiando import datasets
+import matplotlib.pyplot as plt
 
 # Fetching Bouguer anomaly model data (Surfer ASCII grid file)"
 # Will download the archive and save it with the default name
 archive = datasets.fetch_bouguer_alps_egm()
 
 # Load the GRD file and convert in three numpy-arrays (x, y, bouguer)
-x, y, bouguer, shape = gridder.load_surfer(archive, fmt='ascii')
+x, y, bouguer, shape = datasets.load_surfer(archive, fmt='ascii')
 
-mpl.figure()
-mpl.axis('scaled')
-mpl.title("Data loaded from a Surfer ASCII grid file")
-mpl.contourf(y, x, bouguer, shape, 15, cmap=mpl.cm.RdBu_r)
-cb = mpl.colorbar()
-cb.set_label('mGal')
-mpl.xlabel('y points to East (km)')
-mpl.ylabel('x points to North (km)')
-mpl.m2km()
-mpl.show()
+plt.figure()
+plt.title("Data loaded from a Surfer ASCII grid file")
+plt.contourf(y.reshape(shape)/1000, x.reshape(shape)/1000,
+             bouguer.reshape(shape), 15, cmap='RdBu_r')
+plt.colorbar().set_label('mGal')
+plt.xlabel('y points to East (km)')
+plt.ylabel('x points to North (km)')
+plt.show()
